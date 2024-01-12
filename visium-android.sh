21e08a85a02498332b6095879fe5cc0f1bf3681a
ryanputrabaskara <124553317+ryanputrabaskara@users.noreply.github.com>
2024-01-12 16:25:25 +0700

    Update visium-android.sh

--- visium-android.sh
+++ visium-android.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 set -x
 
-# step 1: check and install jq
+# Step 1: Check and install jq
 # Check if jq is installed
-#!/bin/bash
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Installing..."
 
-# Check if Homebrew is installed
-if command -v brew &> /dev/null; then
-    # Check if jq is installed
-    if brew list --formula | grep -q 'jq'; then
-        # Get jq version and display it
-        jq_version=$(jq --version)
-        echo "jq version: $jq_version"
-
-        # Perform additional tasks using jq, for example:
-        cat data.json | jq '.name'
+    # Install jq using Homebrew
+    if command -v brew &> /dev/null; then
+        brew install jq
+        if command -v jq &> /dev/null; then
+            echo "jq has been successfully installed."
+        else
+            echo "Failed to install jq. Please install it manually."
+            exit 1
+        fi
     else
-        echo "jq is not installed. Please install it using 'brew install jq'."
+        echo "Homebrew is not installed. Please install Homebrew first."
+        exit 1
     fi
 else
-    echo "Homebrew is not installed. Please install Homebrew first."
+    echo "jq is already installed."
 fi
 
-
-
-# step 2: get all devide id of Android
+# Step 2: Get all device IDs of Android
 # Make the GET request and capture the JSON response
 response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android \
 -H "Content-Type: application/json" \
@@ -33,17 +32,15 @@ response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android
 # Extract the 'id' attributes from the JSON response and format output with double quotes
 ids=$(echo "$response" | jq -r '[.[] | .deviceId] | map("\"" + . + "\"") | join(",")')
 
-
 # Output the extracted 'id' attributes in the required format
 echo "$ids"
 
 export ids=$ids
 
-
-# step 3: upload APK file and get the id of APK file
+# Step 3: Upload APK file and get the ID of the APK file
 # Set the necessary variables
 API_KEY="2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0"
-FILE_PATH="Simple_Notepad_2.0.1_Apkpure.apk"
+FILE_PATH="Register_2.0_Apkpure.apk"
 API_URL="https://farmdemo.visiumlabs.com/api/v1/apps"
 
 # Make the POST request and capture the response
@@ -52,23 +49,21 @@ response=$(curl -s -X POST "$API_URL" \
 -F "file=@$FILE_PATH")
 
 # Extract the 'id' attribute using jq
-
-appId=$(echo "$response" | grep -o '"id" : [0-9]*' | cut -d':' -f2 | tr -d ' ')
-
+appId=$(echo "$response" | jq -r '.id')
 
 # Output the extracted 'appId'
 echo "The extracted appId is: $appId"
 
 export appId=$appId
 
-# step 4: Install the APK in all Android devices
+# Step 4: Install the APK in all Android devices
 curl -X POST https://farmdemo.visiumlabs.com/api/apk/install \
 -H "Content-Type: application/json" \
 -H "X-VisiumFarm-Api-Key: 2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0" \
 -d '{
   "appList": [
     {
-      "appId": '"$appId"', 
+      "appId": "'"$appId"'",
       "type": "Android"
     }
   ],
